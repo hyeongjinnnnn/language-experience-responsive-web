cff8582911c1182ebf530c7dbc57d1c326f72149
hyeongjinnnnn <bark000104@naver.com>
2023-11-23 03:59:00 +0900

    Implement header menu & background video

--- js/header.js
+++ js/header.js
@@ -5,3 +5,12 @@ menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   headerNav.classList.toggle("active");
 });
+
+for (let i = 1; i <= 4; i++) {
+  const headerLink = document.querySelector(`#headerLink${i}`);
+
+  headerLink.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    headerNav.classList.toggle("active");
+  });
+}
