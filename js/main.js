98f9bf6bb4bd5f6bd448d863efb1cc8faa62f055
hyeongjinnnnn <bark000104@naver.com>
2023-11-28 01:50:19 +0900

    Implement feature

--- js/main.js
+++ js/main.js
@@ -1,27 +1,297 @@
-// 네이버 검색 결과 페이지 URL
-const searchURL = "http://localhost:3000/api/search";
+/* 웹 스크래핑 테이블 추출 */
+const alphabetURL = 'http://localhost:3000/api/alphabet';
+const hiraganaURL = 'http://localhost:3000/api/hiragana';
+const frenchURL = 'http://localhost:3000/api/french';
 
 // 네이버 검색 결과 페이지에서 테이블을 추출하고 동적으로 생성하는 함수
-async function createTableFromNaver() {
+async function createTableFromNaver(url, tableName) {
   try {
     // 네이버 검색 결과 페이지에서 HTML 가져오기
-    const response = await fetch(searchURL);
+    const response = await fetch(url);
     const html = await response.text();
 
     // HTML에서 원하는 테이블 부분 추출
     const parser = new DOMParser();
-    const doc = parser.parseFromString(html, "text/html");
-    const tableElement = doc.querySelector("table.inven");
+    const doc = parser.parseFromString(html, 'text/html');
+    const tableElement = doc.querySelector('table');
 
     // 동적으로 생성한 테이블을 표시할 div 요소 찾기
-    const resultTableContainer = document.getElementById("resultTable");
+    const resultTableContainer = document.getElementById(tableName);
 
     // 동적으로 생성한 테이블을 표시
     resultTableContainer.appendChild(tableElement);
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error);
   }
 }
 
 // 페이지 로드 시 테이블 생성 함수 호출
-window.onload = createTableFromNaver;
+window.onload = function () {
+  const functionsToCall = [
+    //() => createTableFromNaver(alphabetURL, "resultTableAlphabet"),
+    () => createTableFromNaver(hiraganaURL, 'resultTableHiragana'),
+    //() => createTableFromNaver(frenchURL, "resultTableFrench"),
+  ];
+
+  functionsToCall.forEach((func) => func());
+};
+
+// 영어 단어와 뜻 배열
+const english_word = [
+  { word: 'love', meaning: '사랑' },
+  { word: 'face', meaning: '얼굴' },
+  { word: 'lie', meaning: '거짓말' },
+  { word: 'subway', meaning: '지하철' },
+  { word: 'cat', meaning: '고양이' },
+  { word: 'age', meaning: '나이' },
+  { word: 'mushroom', meaning: '버섯' },
+  { word: 'sea', meaning: '바다' },
+  { word: 'color', meaning: '색깔' },
+  { word: 'brother', meaning: '형, 오빠' },
+  { word: 'insect', meaning: '벌레' },
+  { word: 'rain', meaning: '비' },
+  { word: 'flower', meaning: '꽃' },
+  { word: 'person', meaning: '사람' },
+  { word: 'mountain', meaning: '산' },
+];
+/* 랜덤으로 영어 단어 출력 */
+function showRandomEnglishWord() {
+  // 랜덤으로 인덱스 선택
+  const randomIndex = Math.floor(Math.random() * english_word.length);
+
+  // 선택된 단어와 뜻 표시
+  const wordElement = document.getElementById('english_word');
+  const meaningElement = document.getElementById('english_meaning');
+
+  wordElement.textContent = english_word[randomIndex].word;
+  meaningElement.textContent = english_word[randomIndex].meaning;
+}
+showRandomEnglishWord();
+
+const japanese_word = [
+  { word: 'あい', meaning: '사랑' },
+  { word: 'かお', meaning: '얼굴' },
+  { word: 'うそ', meaning: '거짓말' },
+  { word: 'ちかてつ', meaning: '지하철' },
+  { word: 'ねこ', meaning: '고양이' },
+  { word: 'とし', meaning: '나이' },
+  { word: 'きのこ', meaning: '버섯' },
+  { word: 'うみ', meaning: '바다' },
+  { word: 'いろ', meaning: '색깔' },
+  { word: 'あに', meaning: '형, 오빠' },
+  { word: 'むし', meaning: '벌레' },
+  { word: 'あめ', meaning: '비' },
+  { word: 'はな', meaning: '꽃' },
+  { word: 'ひと', meaning: '사람' },
+  { word: 'やま', meaning: '산' },
+];
+/* 랜덤으로 일본어 단어 출력 */
+function showRandomJapaneseWord() {
+  // 랜덤으로 인덱스 선택
+  const randomIndex = Math.floor(Math.random() * japanese_word.length);
+
+  // 선택된 단어와 뜻 표시
+  const wordElement = document.getElementById('japanese_word');
+  const meaningElement = document.getElementById('japanese_meaning');
+
+  wordElement.textContent = japanese_word[randomIndex].word;
+  meaningElement.textContent = japanese_word[randomIndex].meaning;
+}
+showRandomJapaneseWord();
+
+const french_word = [
+  { word: 'amour', meaning: '사랑' },
+  { word: 'visage', meaning: '얼굴' },
+  { word: 'mensonge', meaning: '거짓말' },
+  { word: 'métro', meaning: '지하철' },
+  { word: 'chat', meaning: '고양이' },
+  { word: 'âge', meaning: '나이' },
+  { word: 'champignon', meaning: '버섯' },
+  { word: 'mer', meaning: '바다' },
+  { word: 'couleur', meaning: '색깔' },
+  { word: 'frère', meaning: '형, 오빠' },
+  { word: 'insecte', meaning: '벌레' },
+  { word: 'pluie', meaning: '비' },
+  { word: 'fleur', meaning: '꽃' },
+  { word: 'personne', meaning: '사람' },
+  { word: 'montagne', meaning: '산' },
+];
+/* 랜덤으로 프랑스어 단어 출력 */
+function showRandomFrenchWord() {
+  // 랜덤으로 인덱스 선택
+  const randomIndex = Math.floor(Math.random() * french_word.length);
+
+  // 선택된 단어와 뜻 표시
+  const wordElement = document.getElementById('french_word');
+  const meaningElement = document.getElementById('french_meaning');
+
+  wordElement.textContent = french_word[randomIndex].word;
+  meaningElement.textContent = french_word[randomIndex].meaning;
+}
+showRandomFrenchWord();
+
+/* english translation & listen*/
+async function translateEnglishText() {
+  const inputText = document.getElementById('inputEnglishText').value;
+
+  if (inputText.trim() === '') {
+    alert('번역할 텍스트를 입력하세요.');
+    return;
+  }
+
+  try {
+    const response = await fetch('http://localhost:3000/translateToEnglish', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text: inputText }),
+    });
+
+    const result = await response.json();
+
+    if (result?.translatedText) {
+      document.getElementById('outputEnglishText').value =
+        result.translatedText;
+    } else {
+      alert('번역에 실패했습니다.');
+    }
+  } catch (error) {
+    console.error('에러:', error);
+    alert('번역에 실패했습니다.');
+  }
+}
+
+/* Text To Speech */
+function speakEnglishText() {
+  const text = document.getElementById('outputEnglishText').value;
+
+  if (text.trim() === '') {
+    alert('텍스트가 없습니다.');
+    return;
+  }
+
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.lang = 'en-US';
+  utterance.rate = 0.8;
+
+  // 음성 재생
+  window.speechSynthesis.speak(utterance);
+}
+
+/* japanese translation & listen*/
+async function translateJapaneseText() {
+  const inputText = document.getElementById('inputJapaneseText').value;
+
+  if (inputText.trim() === '') {
+    alert('번역할 텍스트를 입력하세요.');
+    return;
+  }
+
+  try {
+    const response = await fetch('http://localhost:3000/translateToJapanese', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text: inputText }),
+    });
+
+    const result = await response.json();
+
+    if (result?.translatedText) {
+      document.getElementById('outputJapaneseText').value =
+        result.translatedText;
+    } else {
+      alert('번역에 실패했습니다.');
+    }
+  } catch (error) {
+    console.error('에러:', error);
+    alert('번역에 실패했습니다.');
+  }
+}
+
+/* Text To Speech */
+function speakJapaneseText() {
+  const text = document.getElementById('outputJapaneseText').value;
+
+  if (text.trim() === '') {
+    alert('텍스트가 없습니다.');
+    return;
+  }
+
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.lang = 'ja-JP';
+
+  // 음성 재생
+  window.speechSynthesis.speak(utterance);
+}
+
+/* french translation & listen*/
+async function translateFrenchText() {
+  const inputText = document.getElementById('inputFrenchText').value;
+
+  if (inputText.trim() === '') {
+    alert('번역할 텍스트를 입력하세요.');
+    return;
+  }
+
+  try {
+    const response = await fetch('http://localhost:3000/translateToFrench', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text: inputText }),
+    });
+
+    const result = await response.json();
+
+    if (result?.translatedText) {
+      document.getElementById('outputFrenchText').value = result.translatedText;
+    } else {
+      alert('번역에 실패했습니다.');
+    }
+  } catch (error) {
+    console.error('에러:', error);
+    alert('번역에 실패했습니다.');
+  }
+}
+
+/* Text To Speech */
+function speakFrenchText() {
+  const text = document.getElementById('outputFrenchText').value;
+
+  if (text.trim() === '') {
+    alert('텍스트가 없습니다.');
+    return;
+  }
+
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.lang = 'fr-FR';
+
+  // 음성 재생
+  window.speechSynthesis.speak(utterance);
+}
+
+/* 메일 보내기 */
+function sendMail() {
+  var params = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    message: document.getElementById('message').value,
+  };
+
+  const serviceID = 'service_hyeong';
+  const templateID = 'template_flzqocd';
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById('name').value = '';
+      document.getElementById('email').value = '';
+      document.getElementById('message').value = '';
+      alert('메일 전송이 완료되었습니다.');
+    })
+    .catch((err) => console.log(err));
+}
--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors"); // cors 모듈 추가
-const fs = require("fs"); // fs 모듈 추가
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors'); // cors 모듈 추가
+const fs = require('fs'); // fs 모듈 추가
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
 // CORS 설정
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get("/api/search", async (req, res) => {
+app.use(express.static('public'));
+
+/* 영어 알파벳 
+app.get("/api/alphabet", async (req, res) => {
   try {
     const searchURL =
-      "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%ED%9E%88%EB%9D%BC%EA%B0%80%EB%82%98+%ED%91%9C";
+      "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EC%98%81%EC%96%B4+%EC%95%8C%ED%8C%8C%EB%B2%B3+%EB%B0%B0%EC%97%B4";
 
     const response = await axios.get(searchURL);
     const data = response.data;
@@ -23,7 +30,159 @@ app.get("/api/search", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+*/
 
+app.get('/api/hiragana', async (req, res) => {
+  try {
+    const searchURL =
+      'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%ED%9E%88%EB%9D%BC%EA%B0%80%EB%82%98+%ED%91%9C';
+
+    const response = await axios.get(searchURL);
+    const data = response.data;
+
+    res.send(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+/* 프랑스어
+app.get("/api/french", async (req, res) => {
+  try {
+    const searchURL =
+      "https://venglishstudy.tistory.com/entry/%ED%94%84%EB%9E%91%EC%8A%A4%EC%96%B4-%EC%95%8C%ED%8C%8C%EB%B2%B3-%EB%B0%9C%EC%9D%8C%ED%94%84%EB%9E%91%EC%8A%A4%EC%96%B4-%EB%AA%A8%EC%9D%8C%EC%9D%84-%EB%B0%B0%EC%9B%8C%EB%B3%B4%EC%9E%90";
+
+    const response = await axios.get(searchURL);
+    const data = response.data;
+
+    res.send(data);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+*/
+
+//번역 앤드포인트
+app.post('/translateToEnglish', async (req, res) => {
+  const { text } = req.body;
+  const clientId = '9ljWgWQDh8wydwsCK1yR'; // api key id
+  const clientSecret = 'dW4i3ee7Cc'; // api key secret
+  const papagoUrl = 'https://openapi.naver.com/v1/papago/n2mt';
+
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'X-Naver-Client-Id': '9ljWgWQDh8wydwsCK1yR',
+    'X-Naver-Client-Secret': 'pnb82zY9oZ',
+  };
+
+  const data = new URLSearchParams({
+    source: 'ko',
+    target: 'en',
+    text: text,
+  });
+
+  try {
+    const response = await fetch(papagoUrl, {
+      method: 'POST',
+      headers: headers,
+      body: data,
+    });
+
+    if (!response.ok) {
+      throw new Error('API 호출이 실패했습니다.');
+    }
+
+    const result = await response.json();
+    const translatedText = result.message.result.translatedText;
+
+    res.json({ translatedText });
+  } catch (error) {
+    console.error('번역 오류:', error);
+    res.status(500).json({ error: '번역 오류가 발생했습니다.' });
+  }
+});
+
+app.post('/translateToJapanese', async (req, res) => {
+  const { text } = req.body;
+  const clientId = '9ljWgWQDh8wydwsCK1yR'; // api key id
+  const clientSecret = 'dW4i3ee7Cc'; // api key secret
+  const papagoUrl = 'https://openapi.naver.com/v1/papago/n2mt';
+
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'X-Naver-Client-Id': '9ljWgWQDh8wydwsCK1yR',
+    'X-Naver-Client-Secret': 'pnb82zY9oZ',
+  };
+
+  const data = new URLSearchParams({
+    source: 'ko',
+    target: 'ja',
+    text: text,
+  });
+
+  try {
+    const response = await fetch(papagoUrl, {
+      method: 'POST',
+      headers: headers,
+      body: data,
+    });
+
+    if (!response.ok) {
+      throw new Error('API 호출이 실패했습니다.');
+    }
+
+    const result = await response.json();
+    const translatedText = result.message.result.translatedText;
+
+    res.json({ translatedText });
+  } catch (error) {
+    console.error('번역 오류:', error);
+    res.status(500).json({ error: '번역 오류가 발생했습니다.' });
+  }
+});
+
+app.post('/translateToFrench', async (req, res) => {
+  const { text } = req.body;
+  const clientId = '9ljWgWQDh8wydwsCK1yR'; // api key id
+  const clientSecret = 'dW4i3ee7Cc'; // api key secret
+  const papagoUrl = 'https://openapi.naver.com/v1/papago/n2mt';
+
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'X-Naver-Client-Id': '9ljWgWQDh8wydwsCK1yR',
+    'X-Naver-Client-Secret': 'pnb82zY9oZ',
+  };
+
+  const data = new URLSearchParams({
+    source: 'ko',
+    target: 'fr',
+    text: text,
+  });
+
+  try {
+    const response = await fetch(papagoUrl, {
+      method: 'POST',
+      headers: headers,
+      body: data,
+    });
+
+    if (!response.ok) {
+      throw new Error('API 호출이 실패했습니다.');
+    }
+
+    const result = await response.json();
+    const translatedText = result.message.result.translatedText;
+
+    res.json({ translatedText });
+  } catch (error) {
+    console.error('번역 오류:', error);
+    res.status(500).json({ error: '번역 오류가 발생했습니다.' });
+  }
+});
+
+// 서버 시작
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log('서버가 http://localhost:${port} 에서 실행 중입니다.');
 });
